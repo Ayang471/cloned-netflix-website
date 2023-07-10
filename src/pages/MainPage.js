37d2cd52eacdcf32f79d19f42aabe136f31fa500
github-aliceyang <aliyang47@gmail.com>
2023-07-10 16:58:50 -0700

    created main page for home page components

---
+++ src/pages/MainPage.js
@@ -0,0 +1,20 @@
+import React from 'react'
+import ListofOptions from '../components/ListofOptions'
+import PlanBanner from '../components/PlanBanner'
+import Accordions from '../AccordionsComponents/Accordions'
+import Footer from '../components/Footer'
+import HeroSection from '../components/HeroSection'
+
+function MainPage() {
+  return (
+    <div className='main-page'>
+      <HeroSection />
+      <PlanBanner />
+      <ListofOptions />
+      <Accordions />
+      <Footer />
+    </div>
+  )
+}
+
+export default MainPage
