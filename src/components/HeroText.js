58f342ef8b4505c7f2a9940caa25c4d36969ca5e
github-aliceyang <aliyang47@gmail.com>
2023-07-10 17:03:03 -0700

    created hero text section

---
+++ src/components/HeroText.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import '../stylesheets/HeroText.css'
+
+
+
+function HeroText() {
+    return (
+        <div className='HeroText'>
+            <h1>Unlimited movies, TV shows, and more</h1>
+            <p className='P1'>Watch anywhere. Cancel anytime</p>
+    
+        </div>
+    )
+}
+
+export default HeroText
