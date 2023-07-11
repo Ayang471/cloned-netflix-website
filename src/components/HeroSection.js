180518de2466ceab8f58fdbc716b278bec14606e
github-aliceyang <aliyang47@gmail.com>
2023-07-10 17:02:44 -0700

    created the hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,51 @@
+import React, { useState } from 'react'
+
+import '../stylesheets/HeroSection.css'
+import Logo from '../assets/netflixlogo.png'
+import { Link } from 'react-router-dom';
+
+
+function HeroSection() {
+  const [selectedLanguage, setSelectedLanguage] = useState('');
+
+ const handleChange = (event) => {
+  setSelectedLanguage(event.target.value);
+  };
+
+  return (
+    <div className='hero-section'>
+      <div className='hero-container'>
+        <div className="navbar">
+          <nav className="navbar navbar-expand-lg">
+            <div className="container">
+              <Link to='/'><img src={Logo} alt="logo" /></Link>
+              <button
+                className="navbar-toggler"
+                type="button"
+                data-bs-toggle="collapse"
+                data-bs-target="#navbarSupportedContent"
+                aria-controls="navbarSupportedContent"
+                aria-expanded="false"
+                aria-label="Toggle navigation"
+              >
+                <span className="navbar-toggler-icon"></span>
+              </button>
+              <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+                <select id="language-select" value={selectedLanguage} onChange={handleChange}>
+                  <option value="en">English</option>
+                  <option value="es">Spanish</option>
+                </select>
+                <Link to="/SignInPage" className="btn">
+                  Sign In
+                </Link>
+              </div>
+            </div>
+          </nav>
+        </div>
+      </div>
+
+    </div>
+  )
+}
+
+export default HeroSection
