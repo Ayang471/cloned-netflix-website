0749e090d8820ec0b3cf2a501be8b02a620f2534
github-aliceyang <aliyang47@gmail.com>
2023-07-12 12:18:31 -0700

    imported language component

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,18 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import HeroText from './HeroText'
 import '../stylesheets/HeroSection.css'
 import Logo from '../assets/netflixlogo.png'
 import { Link } from 'react-router-dom';
 import { Hide } from '@chakra-ui/react'
+import LanguageComponent from '../LanguageComponent';
+
 
 
 function HeroSection() {
-  const [selectedLanguage, setSelectedLanguage] = useState('');
-
- const handleChange = (event) => {
-  setSelectedLanguage(event.target.value);
-  };
-
   return (
     <div className='hero-section'>
       <div className='hero-container'>
@@ -22,12 +18,7 @@ function HeroSection() {
               <Link to='/'><img src={Logo} alt="logo" className='logo' /></Link>
               <div className="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
                 <Hide breakpoint='(max-width: 482px)'>
-                    <select id="language-select" value={selectedLanguage} onChange={handleChange}>
-                  <option value="en" >
-                    English
-                    </option>
-                  <option value="es">Spanish</option>
-                </select>
+                   <LanguageComponent />
                 </Hide>
                 <Link to="/SignInPage" className="btn">
                   Sign In
