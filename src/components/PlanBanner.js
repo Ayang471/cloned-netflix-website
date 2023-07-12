d317da495fa36be454d2c65aa5876992a09de981
github-aliceyang <aliyang47@gmail.com>
2023-07-12 12:09:34 -0700

    updated plan banner

--- src/components/PlanBanner.js
+++ src/components/PlanBanner.js
@@ -9,16 +9,18 @@ function PlanBanner() {
     return (
         <div className='PlanBanner'>
             <div className='container'>
-                <div className='center-text'>
-                    <img src={popcorn} alt='popcorn' />
-                </div>
-                <div className='center-text'>
-                    <h2>The Netflix you love for just $6.99.</h2>
-                    <p>Get the Standard with ads plan.</p>
-                    <a href="/" target="_blank">
-                        Learn More
-                        <FontAwesomeIcon icon={faChevronRight} style={{ color: "#458DF3", marginLeft: '6px', background: 'transparent' }} />
-                    </a>
+                <div className='row'>
+                    <div className='col popcorn'>
+                        <img src={popcorn} alt='popcorn' />
+                    </div>
+                    <div className='col center-text'>
+                        <h2>The Netflix you love for just $6.99.</h2>
+                        <p>Get the Standard with ads plan.</p>
+                        <a href="/" target="_blank">
+                            Learn More
+                            <FontAwesomeIcon icon={faChevronRight} style={{ color: "#458DF3", marginLeft: '6px', background: 'transparent' }} />
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
