00e83c0960b9b2ae1a3cc294e93c5305d33e32cb
github-aliceyang <aliyang47@gmail.com>
2023-07-21 15:33:03 -0700

    updated app

--- src/App.js
+++ src/App.js
@@ -2,12 +2,13 @@ import './App.css';
 import React from 'react';
 import { Routes, Route } from 'react-router-dom'
 import SignInPage from './pages/SignInPage'
-import MainPage from  './pages/MainPage'
+import MainPage from './pages/MainPage'
+
 
 function App() {
 
   return (
-    <div className="App">
+    <div className="App" >
       <Routes>
         <Route path="/" element={<MainPage />}></Route>
         <Route path="/SignInPage" element={<SignInPage />}></Route>
